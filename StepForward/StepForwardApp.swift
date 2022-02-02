db13c758ba835e425cbcc0b981cc8e7c44d02993
Erika Frison <enfrison@gmail.com>
2022-02-02 09:05:21 -0500

    Initial Commit

---
+++ StepForward/StepForwardApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepForwardApp.swift
+//  StepForward
+//
+//  Created by Erika Frison on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct StepForwardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
