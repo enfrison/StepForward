cabe8dd670f96fd0e9fbf345104264ddc86f47cc
Cukeyra Blackshear <cukeyra@yahoo.com>
2022-02-03 11:15:28 -0500

    Change color and text

--- StepForward/ContentView.swift
+++ StepForward/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack{
+            Color(.blue).edgesIgnoringSafeArea(.all)
+        Text("I'm doing it!")
             .padding()
     }
 }
-
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
