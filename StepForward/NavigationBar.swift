67972b7ac6630073203a018b3c717d0b5b11aa56
Erika Frison <enfrison@gmail.com>
2022-02-04 10:43:47 -0500

    Navigation Bar Added

--- StepForward/NavigationBar.swift
+++ StepForward/NavigationBar.swift
@@ -21,6 +21,7 @@ struct NavigationBar: View {
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .padding(.leading, 20)
         }
+        
         .frame(height: 70)
         .frame(maxHeight: .infinity, alignment: .top)
         
