5302b42ecab893b1eb9bca21633d8cddea9eb5f1
Leteef Wyatt <wyattleteef@gmail.com>
2022-02-21 08:43:20 -0500

    Adding final colors.

--- StepForward/ResourcesView.swift
+++ StepForward/ResourcesView.swift
@@ -9,14 +9,16 @@ import SwiftUI
 
 struct ResourcesView: View {
     var body: some View {
-        NavigationView {
+        NavigationView {ZStack{
+            Color("Blue")
+            RoundedRectangle(cornerRadius: 38).frame(width: 360, height: 500 ).position(x:196, y: 470)
             Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
                 .navigationTitle("Resources")
                 .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
-
+}
 struct ResourcesView_Previews: PreviewProvider {
     static var previews: some View {
         ResourcesView()
