955e80ae2b9076cd75b5c13eea6b49de7a0a2009
Leteef Wyatt <wyattleteef@gmail.com>
2022-02-21 08:51:45 -0500

    Resource Page color done.

--- StepForward/ResourcesView.swift
+++ StepForward/ResourcesView.swift
@@ -11,7 +11,7 @@ struct ResourcesView: View {
     var body: some View {
         NavigationView {ZStack{
             Color("Blue")
-            RoundedRectangle(cornerRadius: 38).frame(width: 360, height: 500 ).position(x:196, y: 470)
+            RoundedRectangle(cornerRadius: 38).frame(width: 360, height: 700 ).position(x:196, y: 370).foregroundColor(Color("Dark Blue"))
             Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
                 .navigationTitle("Resources")
                 .navigationBarTitleDisplayMode(.inline)
